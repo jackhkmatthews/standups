dcaf3e45dd8b031dbc2ff0811f7516c1b07ffce7
Jack Matthews <jackhkmatthews@gmail.com>
2017-06-20 19:00:15 +0100

    extracting formation from txt

--- parser/app.js
+++ parser/app.js
@@ -1,16 +1,17 @@
 let Parser = require('simple-text-parser');
 let parser = new Parser();
-const fs = require('fs')
-
-parser.addRule(/:.*?\|/ig, function(formation) {
-  return { type: 'formation', text: formation };
-});
+const fs = require('fs');
 
 const filename = 'week.txt';
 
-let fileAsString = '';
-
 fs.readFile(filename, 'utf8', function(err, data) {
+  
+  parser.addRule(/:.*?\|/ig, function(formation) {
+    return { type: 'formation', text: formation };
+  });
+
+
+  let fileAsString = '';
   if (err) throw err;
   fileAsString = data;
   let json = parser.toTree(fileAsString);
@@ -24,4 +25,28 @@ fs.readFile(filename, 'utf8', function(err, data) {
   const formation = json[0].match(/(\w+)/ig);
   console.log(formation);
 });
+
+
+// fs.readFile(filename, 'utf8', function(err, data) {
+
+//   parser.addRule(/:.*?\|/ig, function(formation) {
+//     return { type: 'formation', text: formation };
+//   });
+
+//   const filename = 'week.txt';
+
+//   let fileAsString = '';
+//   if (err) throw err;
+//   fileAsString = data;
+//   let json = parser.toTree(fileAsString);
+//   json = json.filter((element) => {
+//     return (element.type === 'formation');
+//   })
+//   json = json.map(element => {
+//     return element.text;
+//   });
+//   console.log(json[0]);
+//   const formation = json[0].match(/(\w+)/ig);
+//   console.log(formation);
+// });
 
