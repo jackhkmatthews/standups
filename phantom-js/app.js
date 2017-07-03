853864ae9d024c1b1ae00d8109beebb525fba163
Jack Matthews <jackhkmatthews@gmail.com>
2017-07-03 13:02:32 +0100

    finally got svg rendering server side with jsdom

--- phantom-js/app.js
+++ phantom-js/app.js
@@ -2,14 +2,34 @@ const fs = require('fs');
 const page = require('webpage').create();
 const url = 'file://' + fs.absolute('./index.html');
 
-const drawer = function() {
-  const body = document.getElementById('body');
-  body.innerHTML = 'Paragraph changed!';
-  const doc = document.getElementById('html');
-  return doc.outerHTML;
+var svgDrawer = function() {
+  var svgContainer = document.getElementById("svg");
+  var paper = Raphael(svgContainer, 640, 480);
+  paper
+    .rect(0, 0, 640, 480, 10)
+    .attr({
+      fill: '#fff',
+      stroke: 'none'
+    });
+  var circle = paper
+              .circle(320, 240, 60)
+              .attr({
+                fill: '#223fa3',
+                stroke: '#000',
+                'stroke-width': 80,
+                'stroke-opacity': 0.5
+              });
+  paper
+    .rect(circle.attr('cx') - 10, circle.attr('cy') - 10, 20, 20)
+    .attr({
+      fill: '#fff',
+      stroke: 'none'
+    });
+
+  return svgContainer.innerHTML;
 };
 
 page.open(url, function (status) {
-  console.log(page.evaluate(drawer));
-  phantom.exit();
+   console.log(page.evaluate(svgDrawer));
+   phantom.exit();
 });
